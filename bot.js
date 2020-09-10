36c5ad1e75f54ee5c8f2376a87a47689305757bf
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2020-09-10 22:06:27 +0530

    init bot: store names from user to memory db

---
+++ bot.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const { default: Telegraf } = require('telegraf')
+const session = require('telegraf/session')
+const WizardScene = require('telegraf/scenes/wizard')
+const Stage = require('telegraf/stage')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+// in memory db to store names
+var names = []
+
+
+const startGame = new WizardScene('start-game',
+    async (ctx) => {
+        names = []
+        await ctx.reply(`Enter people's name you want to begin with newline.\n\nEg.\n\tSwapnil\n\tNisharg\n\tMeet\n\tRajan\n\nAnd send. (Don't add tabs and spaces)`)
+        return ctx.wizard.next()
+    },
+    async (ctx) => {
+        names = ctx.message.text.split('\n')
+        await ctx.reply(`Adding ${names.length} people to memory database.`)
+        await ctx.reply("You can now use /spin to continue game and /stop to stop the game.")
+        ctx.scene.leave()
+    }
+)
+
+const stage = new Stage([startGame])
+// middlewares
+bot.use(session())
+bot.use(stage.middleware())
+
+bot.start((ctx) => {
+    ctx.scene.enter('start-game')
+})
+
+bot.launch()
+
+module.exports = bot
